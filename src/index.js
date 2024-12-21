a6f098888dfda333a38a052ba1a8d6040beb8de8
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-12-21 23:30:58 +0100

    Render main page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import "./css/normalize.css";
 import "./css/style.css";
+
+import { appendHomePage } from "./pages/home.js";
+
+const mainContainer = document.querySelector("#content");
+
+appendHomePage(mainContainer);
