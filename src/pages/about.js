ad7965baba3b303408e004d8469d1b3cb760c285
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-12-24 19:35:14 +0100

    Add location and open hours

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,8 @@ import {
   appendChildren,
 } from "./home.js";
 
+import grillImg from "../images/grill.jpg";
+
 const title = "All Types of Burgers, One Incredible Spot";
 
 const aboutInfo1 =
@@ -13,13 +15,27 @@ const aboutInfo1 =
 const aboutInfo2 =
   "Our passion for quality means every burger is made to order, topped with handpicked produce and house-made sauces. Pair it with crispy fries and a cold drink for a meal that’s simply unforgettable.";
 
+const location = "Location: Denver Street, Pixel Place";
+
+const openHours =
+  "We’re open seven days a week from 11 AM to 7 PM—or until we sell out. Come taste the difference!";
+
 export function appendAboutPage(div) {
   const aboutContainer = renderDiv("about_container", "");
+  const imgOpenHoursContainer = renderDiv("img_open_container", "");
+  const img_open = [
+    renderImage("about_image", grillImg),
+    renderDiv("openHours", openHours),
+  ];
+  appendChildren(imgOpenHoursContainer, ...img_open);
   const info = [
     renderDiv("about_title", title),
     renderDiv("", aboutInfo1),
     renderDiv("", aboutInfo2),
+    renderDiv("location", location),
+    imgOpenHoursContainer,
   ];
   appendChildren(aboutContainer, ...info);
   div.appendChild(aboutContainer);
+  renderFooter(div);
 }
