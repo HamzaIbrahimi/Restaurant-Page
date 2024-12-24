f234109660d45ab7dc7161151a30c4fb36e5df57
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2024-12-24 18:33:28 +0100

    Render about page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import "./css/normalize.css";
 import "./css/style.css";
 
 import { appendHomePage } from "./pages/home.js";
-
+import { appendAboutPage } from "./pages/about.js";
 const mainContainer = document.querySelector("#content");
-
 appendHomePage(mainContainer);
+
+const mainButtons = [
+  document.querySelector("#home"),
+  document.querySelector("#about"),
+  document.querySelector("#menu"),
+];
+
+function cleanUp() {
+  mainContainer.innerHTML = "";
+  mainButtons.forEach((button) => {
+    button.style.textDecoration = "none";
+  });
+}
+
+mainButtons[0].addEventListener("click", () => {
+  cleanUp();
+  appendHomePage(mainContainer);
+  mainButtons[0].style.textDecoration = "overline";
+});
+
+mainButtons[1].addEventListener("click", () => {
+  cleanUp();
+  appendAboutPage(mainContainer);
+  mainButtons[1].style.textDecoration = "overline";
+});
+mainButtons[2].addEventListener("click", () => {});
